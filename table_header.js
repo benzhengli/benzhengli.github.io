c050598c77876d9931175495a47cf73e1bb352ac
benzhengli <50790546+benzhengli@users.noreply.github.com>
2022-11-22 15:32:24 +0800

    Update table_header.js

--- table_header.js
+++ table_header.js
@@ -4,24 +4,21 @@ document.write("<tr>");
 document.write("<td> <img src=\"Images/xinyu_chen.png\" alt=\"Xinyu Chen\"");
 document.write("width=\"162.6\" height=\"200\"> <\/td>");
 document.write("<td>");
-document.write("<h1>Xinyu Chen</h1>");
+document.write("<h1>Ben-Zheng Li</h1>");
 document.write("<br>");
 document.write("<b>Ph.D. Candidate</b>");
 document.write("<br>");
 
-document.write("<a href=\"https:\/\/www.umontreal.ca\/en\">University of Montreal<\/a></b>");
+document.write("<a href=\"https://www.uestc.edu.cn/">University of Electronic Science and Technology of China<\/a></b>");
 
 document.write("<br>");
 document.write("<br>");
 document.write("<b>Contact info:</b><br>");
-document.write(" Montreal, Quebec, Canada. <br>");
-document.write("E-mail: chenxy346@gmail.com <br>");
+document.write(" Chengdu, Sichuan, China. <br>");
+document.write("E-mail: lbz1604179601@126.com <br>");
 document.write("<br>");
 document.write("&nbsp;");
-document.write("<a href=\"https:\/\/scholar.google.com\/citations?user=mCrW04wAAAAJ&hl\"><img src=\"Images\/scholar.png\" alt=\"Google Scholar\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
 document.write("<a href=\"https:\/\/github.com\/xinychen\"><img src=\"Images\/GitHub-Mark-120px-plus.png\" alt=\"GitHub\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
-// document.write("<a href=\"https:\/\/www.zhihu.com\/people\/its312\/posts\"> <img src=\"Images\/zhihu.png\" alt=\"Zhihu\"  border=\"0\" height=\"25\"><\/a> &nbsp");
-document.write("<a href=\"https:\/\/twitter.com\/chenxy346\/\"> <img src=\"Images\/twitter.png\" alt=\"Twitter\"  border=\"0\" height=\"25\"><\/a><br>");
 document.write("<\/td>");
 document.write("<\/tr>");
 document.write("<\/tbody>");
