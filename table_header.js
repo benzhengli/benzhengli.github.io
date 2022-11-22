d81ab4bdc21a96d87b72fb9d276971078dcd9d12
benzhengli <50790546+benzhengli@users.noreply.github.com>
2022-11-22 15:23:31 +0800

    Add files via upload

---
+++ table_header.js
@@ -0,0 +1,28 @@
+document.write("<table width=\"650\" border=\"0\" height=\"184\">");
+document.write("<tbody>");
+document.write("<tr>");
+document.write("<td> <img src=\"Images/xinyu_chen.png\" alt=\"Xinyu Chen\"");
+document.write("width=\"162.6\" height=\"200\"> <\/td>");
+document.write("<td>");
+document.write("<h1>Xinyu Chen</h1>");
+document.write("<br>");
+document.write("<b>Ph.D. Candidate</b>");
+document.write("<br>");
+
+document.write("<a href=\"https:\/\/www.umontreal.ca\/en\">University of Montreal<\/a></b>");
+
+document.write("<br>");
+document.write("<br>");
+document.write("<b>Contact info:</b><br>");
+document.write(" Montreal, Quebec, Canada. <br>");
+document.write("E-mail: chenxy346@gmail.com <br>");
+document.write("<br>");
+document.write("&nbsp;");
+document.write("<a href=\"https:\/\/scholar.google.com\/citations?user=mCrW04wAAAAJ&hl\"><img src=\"Images\/scholar.png\" alt=\"Google Scholar\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
+document.write("<a href=\"https:\/\/github.com\/xinychen\"><img src=\"Images\/GitHub-Mark-120px-plus.png\" alt=\"GitHub\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
+// document.write("<a href=\"https:\/\/www.zhihu.com\/people\/its312\/posts\"> <img src=\"Images\/zhihu.png\" alt=\"Zhihu\"  border=\"0\" height=\"25\"><\/a> &nbsp");
+document.write("<a href=\"https:\/\/twitter.com\/chenxy346\/\"> <img src=\"Images\/twitter.png\" alt=\"Twitter\"  border=\"0\" height=\"25\"><\/a><br>");
+document.write("<\/td>");
+document.write("<\/tr>");
+document.write("<\/tbody>");
+document.write("<\/table>");
