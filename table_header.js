01ebcfcf4227966804d84720aa3facab0df9640b
Ben-Zheng Li <50790546+benzhengli@users.noreply.github.com>
2026-03-12 16:47:07 +0800

    Update table_header.js

--- table_header.js
+++ table_header.js
@@ -6,17 +6,17 @@ document.write("width=\"350\" height=\"250\"> <\/td>");
 document.write("<td>");
 document.write("<h1>Ben-Zheng Li (李本正)</h1>");
 document.write("<br>");
-document.write("<b>Ph.D. Candidate</b>");
+document.write("<b>Associate Professor</b>");
 document.write("<br>");
 
-document.write("<a href=\"https:\/\/www.uestc.edu.cn\"> University of Electronic Science and Technology of China <\/a></b>");
+document.write("<a href=\"https://maths.ccnu.edu.cn/"> School of Mathematics and Statistics, Central China Normal University <\/a></b>");
               
                
 document.write("<br>");
 document.write("<br>");
 document.write("<b>Contact info:</b><br>");
-document.write(" Chengdu, Sichuan, China. <br>");
-document.write("E-mail: lbz1604179601@126.com, e1495946@u.nus.edu <br>");
+document.write(" Wuhan, Hubei, China. <br>");
+document.write("E-mail: lbz1604179601@ccnu.edu.cn, lbz1604179601@126.com<br>");
 document.write("<br>");
 document.write("&nbsp;");
 document.write("<a href=\"https://scholar.google.com/citations?hl=en&user=pXU2ncUAAAAJ&view_op=list_works&gmla=AJsN-F67h7paSgOvSMBZ6ntHaz0EriF3wdjLA39Iavpb_ywY90kAcAojuehiY1dxwVRZfS6E2RjZlSOrJXRwNW4iylxFynymZWzYLo9L7jDVGwGgJKWkakN5i1wqHE7YkUjofT6B_6uCkDPQOAVlLklQMGPL8Pvqkg\"><img src=\"Images\/scholar.png\" alt=\"GitHub\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
