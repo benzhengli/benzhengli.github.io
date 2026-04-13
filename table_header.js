c7f4363e3a925e150edd75a6a44580d3781ff255
Ben-Zheng Li <50790546+benzhengli@users.noreply.github.com>
2026-04-13 23:32:08 +0800

    Update table_header.js

--- table_header.js
+++ table_header.js
@@ -1,25 +1,36 @@
 document.write("<table width=\"850\" border=\"0\" height=\"184\">");
 document.write("<tbody>");
 document.write("<tr>");
-document.write("<td width=\"350\"> <img src=\"Images/athens-lbz.JPG\" alt=\"Ben-Zheng Li\" width=\"320\" height=\"250\"> <\/td>");
-document.write("<td>");
-document.write("<h1>Ben-Zheng Li (李本正)<\/h1>");
-document.write("<br>");
-document.write("<b>Assistant Professor<\/b>");
-document.write("<br>");
-// 注意这里的引号转义和标签匹配
-document.write("<a href=\"https://maths.ccnu.edu.cn/\"> School of Mathematics and Statistics <\/a>");
-document.write("<br>");
+document.write("<td width=\"350\" style=\"vertical-align: top;\"> <img src=\"Images/athens-lbz.JPG\" alt=\"Ben-Zheng Li\" width=\"320\" height=\"250\"> <\/td>");
+
+// 1. 在这里的 <td> 加入了 line-height (行高) 和 vertical-align (顶部对齐)
+document.write("<td style=\"line-height: 1.8; vertical-align: top; padding-top: 10px;\">");
+
+// 姓名标题
+document.write("<h1 style=\"margin-top: 0; margin-bottom: 15px;\">Ben-Zheng Li (李本正)<\/h1>");
+
+// 2. 将“职位与学校”划分为一个区块，并增加底部间距 (margin-bottom)
+document.write("<div style=\"margin-bottom: 20px;\">");
+document.write("<b>Assistant Professor<\/b><br>");
+document.write("<a href=\"https://maths.ccnu.edu.cn/\"> School of Mathematics and Statistics <\/a><br>");
 document.write("<a href=\"https://www.ccnu.edu.cn/\"> Central China Normal University <\/a>");
-document.write("<br>");
+document.write("<\/div>");
+
+// 3. 将“联系方式”划分为一个区块，同样增加底部间距
+document.write("<div style=\"margin-bottom: 20px;\">");
 document.write("<b>Contact info:<\/b><br>");
-document.write(" Wuhan, Hubei, China. <br>");
+document.write("Wuhan, Hubei, China. <br>");
 document.write("Office: Room 306, Math. Building<br>");
-document.write("E-mail: mathlibz@ccnu.edu.cn, lbz1604179601@126.com<br>");
-document.write("&nbsp;");
-document.write("<a href=\"https://scholar.google.com/citations?user=pXU2ncUAAAAJ&hl=en\"><img src=\"Images/scholar.png\" alt=\"Scholar\" width=\"25\" height=\"25\" border=\"0\"> <\/a> &nbsp;");
-document.write("<a href=\"https://www.researchgate.net/profile/Ben-Zheng-Li-2?ev=hdr_xprf\"><img src=\"Images/Researchgate.jpg\" alt=\"Researchgate\" width=\"25\" height=\"25\" border=\"0\"> <\/a> &nbsp;");
+document.write("E-mail: mathlibz@ccnu.edu.cn, lbz1604179601@126.com");
+document.write("<\/div>");
+
+// 4. 图标区块
+document.write("<div>");
+document.write("<a href=\"https://scholar.google.com/citations?user=pXU2ncUAAAAJ&hl=en\"><img src=\"Images/scholar.png\" alt=\"Scholar\" width=\"25\" height=\"25\" border=\"0\" style=\"margin-right: 10px;\"> <\/a>");
+document.write("<a href=\"https://www.researchgate.net/profile/Ben-Zheng-Li-2?ev=hdr_xprf\"><img src=\"Images/Researchgate.jpg\" alt=\"Researchgate\" width=\"25\" height=\"25\" border=\"0\" style=\"margin-right: 10px;\"> <\/a>");
 document.write("<a href=\"https://github.com/benzhengli\"><img src=\"Images/GitHub-Mark-120px-plus.png\" alt=\"GitHub\" width=\"25\" height=\"25\" border=\"0\"> <\/a>");
+document.write("<\/div>");
+
 document.write("<\/td>");
 document.write("<\/tr>");
 document.write("<\/tbody>");
